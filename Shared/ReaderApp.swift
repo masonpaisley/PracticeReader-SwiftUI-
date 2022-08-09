ca0a40372843383fecf7060780c85526c8db80c6
masonpaisley <masonpaisleyy@gmail.com>
2022-08-10 00:06:41 +0800

    Initial Commit

---
+++ Shared/ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderApp.swift
+//  Shared
+//
+//  Created by jinzhao wang on 2022/8/10.
+//
+
+import SwiftUI
+
+@main
+struct ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
